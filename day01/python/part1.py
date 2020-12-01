cdf1f03408f5037f517a9252606023b37a17b4ab
mrabe <mrabe@uni-bremen.de>
2020-12-01 21:58:48 +0100

    Day 1 solved

---
+++ day01/python/part1.py
@@ -0,0 +1,19 @@
+with open("../input01.txt") as file:
+    data = [int(x) for x in file.readlines()]
+
+def findTwo(target, data):
+    for value in data:
+        difference = target - value
+        if difference in data:
+            return(difference * value);
+    return False
+
+def findThree(target, data):
+    for value in data:
+        difference = target - value
+        result = findTwo(difference, data)
+        if result:
+            return value * result
+
+print("Part 1: " + str(findTwo(2020, data)))
+print("Part 2: " + str(findThree(2020, data)))
