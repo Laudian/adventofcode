5faebb735a86df3ee818960c6a36354790e30c54
mrabe <mrabe@uni-bremen.de>
2020-12-15 00:04:52 +0100

    2015_19

---
+++ 2015_19/part1.py
@@ -0,0 +1,20 @@
+target = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
+
+with open("input.txt") as file:
+    data = [line.strip("\n") for line in file.readlines()]
+
+produktionen = {}
+results = []
+
+import re
+
+for line in data:
+    left, right = line.split(" => ")
+    produktionen[left] = right
+    starts = [m.start() for m in re.finditer(left, target)]
+    for start in starts:
+        end = start + len(left)
+        result = target[:start] + right + target[end:]
+        results.append(result)
+
+print("Part 1: " + str(len(set(results))))
