5f5b6451acf7c32709c3192f3f8874cba26210a5
mrabe <mrabe@uni-bremen.de>
2020-12-16 00:05:26 +0100

    day15

--- day15/python/part1.py
+++ day15/python/part1.py
@@ -1,26 +1,40 @@
 import time
-import itertools
 
 data = [13,16,0,12,15,1]
 
-start = time.time()
-
-lastuse = list(itertools.repeat([], 30000000))
+lastuse = [0] * 30000000
 usecount = [0] * 30000000
 
-print(time.time() - start)
-
 for index, nr in enumerate(data):
-    lastuse[nr] += [index]
+    lastuse[nr] = index
     usecount[nr] += 1
-    lastnr = nr
+    newnr = nr
 
-for x in range(len(data), 30000000):
-    lastnr = 0 if usecount[lastnr] < 2 else (x - 1) - lastuse[lastnr][-2]
-    lastuse[lastnr] += [x]
+for x in range(len(data), 2020):
+    lastnr = newnr
+    newnr = 0 if usecount[lastnr] == 0 else (x-1) - lastuse[lastnr]
+    lastuse[lastnr] = x - 1
     usecount[lastnr] += 1
 
-print("Part 1: " + str(lastnr))
+print("Part 1: " + str(newnr))
+
+start = time.time()
+
+lastuse = [0] * 30000000
+usecount = [0] * 30000000
+
+for index, nr in enumerate(data):
+    lastuse[nr] = index
+    usecount[nr] += 1
+    newnr = nr
+
+for x in range(len(data), 30000000):
+    lastnr = newnr
+    newnr = 0 if usecount[lastnr] == 0 else (x-1) - lastuse[lastnr]
+    lastuse[lastnr] = x - 1
+    usecount[lastnr] += 1
+
+print("Part 2: " + str(newnr))
 
 end = time.time()
 print(end - start)
