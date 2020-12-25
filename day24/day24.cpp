0f6cb05d2994486d5510e394da80b6c3594c48c1
mrabe <mrabe@uni-bremen.de>
2020-12-25 14:34:46 +0100

    day24 cpp - not working

---
+++ day24/day24.cpp
@@ -0,0 +1,173 @@
+//
+// Created by Micha on 25.12.2020.
+//
+
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <list>
+
+struct Coord
+{
+    int x, y;
+    Coord(int x, int y) : x(x), y(y) {}
+    friend bool operator==(const Coord& coord1, const Coord& coord2)
+    {
+        return coord1.x == coord2.x and coord1.y == coord2.y;
+    }
+    friend bool operator<(const Coord& coord1, const Coord& coord2)
+    {
+        if (coord1.x != coord2.x)
+        {
+            return coord1.x < coord2.x;
+        }
+        else
+        {
+            return coord1.y < coord2.y;
+        }
+    }
+
+};
+
+struct hash_coord
+    {
+        size_t operator()(const Coord & coord) const
+        {
+            return std::hash<int>()(coord.x) ^ std::hash<int>()(coord.y);
+        }
+    };
+
+typedef std::map<Coord, std::array<bool, 2>> ordered_tiles;
+//typedef std::map<Coord, std::array<bool, 2>, hash_coord, std::equal_to<>> ordered_tiles;
+
+unsigned int countSubstring(std::string str, std::string sub)
+{
+    size_t pos = 0;
+    unsigned int count = 0;
+    while (((pos = str.find(sub, pos)) != str.npos))
+    {
+        ++count;
+        ++pos;
+    }
+    return count;
+}
+
+Coord convertDirToXY(std::string dir)
+{
+    unsigned int nw = countSubstring(dir, "nw");
+    unsigned int ne = countSubstring(dir, "ne");
+    unsigned int sw = countSubstring(dir, "sw");
+    unsigned int se = countSubstring(dir, "se");
+    unsigned int w = countSubstring(dir, "w") - nw - sw;
+    unsigned int e = countSubstring(dir, "e") - ne - se;
+    unsigned int z = nw - se;
+    unsigned int x = e - w - z;
+    unsigned int y = ne - sw + z;
+    return Coord(x, y);
+}
+
+int countNeighbours(Coord tile, ordered_tiles& tiles, bool current)
+{
+    // Nachbarn berechnen
+    std::array<Coord, 6> coords =
+    {
+    Coord(tile.x+1, tile.y),        // e
+    Coord(tile.x-1, tile.y),        // se
+    Coord(tile.x, tile.y+1),        // sw
+    Coord(tile.x+1, tile.y-1),   // w
+    Coord(tile.x, tile.y-1),        // nw
+    Coord(tile.x-1, tile.y+1)    // ne
+    };
+
+    int count = 0;
+    for (auto coord: coords)
+    {
+        // Überprüfe, ob Nachbar bereits existiert
+        if (tiles.find(coord) != tiles.end())
+        {
+            count+= tiles[coord][current];
+        }
+        // Wenn nicht wird der Nachbar weiß erstellt
+        else //if (create)
+        {
+            tiles[coord] = {false, false};
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ordered_tiles tiles;
+    bool current = false;
+
+
+    // Part 1
+    std::ifstream myfile("day24/example.txt");
+    for( std::string line; getline( myfile, line ); )
+    {
+        Coord tile = convertDirToXY(line);
+        // Falls die Tile bereits existiert wird die Farbe geflippt
+        if (tiles.find(tile) != tiles.end())
+        {
+            tiles[tile][current] = not tiles[tile][current];
+        }
+        // Ansonsten wird die Tile mit Farbe schwarz erstellt
+        else
+        {
+            tiles[tile] = {true, true};
+        }
+    }
+
+    int count = 0;
+    for (auto key_value : tiles)
+    {
+        count += key_value.second[current];
+    }
+    std::cout << "Part 1: " << count << std::endl;
+
+    // Part 2
+    // Einmal die Nachbarn aller Tiles erstellen
+    for (auto tile : tiles)
+    {
+        countNeighbours(tile.first, tiles, current);
+    }
+
+    for (int i=0; i<1; i++)
+    {
+        // Items aus der Map kopieren
+        std::list<Coord> list;
+        for(auto key_value : tiles)
+        {
+            list.emplace_back(key_value.first);
+        }
+        std::cout << tiles.size() << std::endl;
+
+        for (auto key : list)
+        {
+            bool color = tiles[key][current];
+            int counter = countNeighbours(key, tiles, current);
+            if (color and (counter == 0 or counter > 2))
+            {
+                tiles[key][not current] = false;
+            }
+            else if ((not color) and counter == 2)
+            {
+                tiles[key][not current] = true;
+            }
+            else
+            {
+                tiles[key][not current] = color;
+            }
+        }
+        current = not current;
+    }
+
+    count = 0;
+    for (auto key_value : tiles)
+    {
+        count += key_value.second[current];
+    }
+    std::cout << "Part 2: " << count << std::endl;
+}
