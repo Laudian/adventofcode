772fe11815727d4c1f3faea61dd85ced857e0948
mrabe <mrabe@uni-bremen.de>
2021-12-06 17:22:14 +0100

    day6: changed list for deque

--- 2021/06/day06_01.py
+++ 2021/06/day06_01.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 with open("input.txt") as file:
     puzzle = [int(x) for x in file.read().split(",")]
 
@@ -6,12 +8,12 @@ cycle = 6
 
 
 def simulate(steps: int):
-    fish = [0]*(max_timer+1)
+    fish = deque([0]*(max_timer+1))
     for nr in puzzle:
         fish[nr] += 1
 
     for n in range(steps):
-        fish.append(fish.pop(0))
+        fish.rotate(-1)
         fish[cycle] += fish[-1]
 
     print(f"Fish after {steps:>3} days: " + str(sum(fish)))
